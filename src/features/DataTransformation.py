ab2af60522237d228931a793a6bf4cf2018da7ed
Harsh Upadhyay <86427322+haaaarsh4@users.noreply.github.com>
2026-02-22 21:40:51 -0500

    Update DataTransformation.py

--- src/features/DataTransformation.py
+++ src/features/DataTransformation.py
@@ -1,21 +1,8 @@
-##############################################################
-#                                                            #
-#    Mark Hoogendoorn and Burkhardt Funk (2017)              #
-#    Machine Learning for the Quantified Self                #
-#    Springer                                                #
-#    Chapter 3                                               #
-#                                                            #
-##############################################################
-
-# Updated by Dave Ebbelaar on 22-12-2022
-
 from sklearn.decomposition import PCA
 from scipy.signal import butter, lfilter, filtfilt
 import copy
 import pandas as pd
 
-# This class removes the high frequency data (that might be considered noise) from the data.
-# We can only apply this when we do not have missing values (i.e. NaN).
 class LowPassFilter:
     def low_pass_filter(
         self,
@@ -26,8 +13,6 @@ class LowPassFilter:
         order=5,
         phase_shift=True,
     ):
-        # http://stackoverflow.com/questions/12093594/how-to-implement-band-pass-butterworth-filter-with-scipy-signal-butter
-        # Cutoff frequencies are expressed as the fraction of the Nyquist frequency, which is half the sampling frequency
         nyq = 0.5 * sampling_frequency
         cut = cutoff_frequency / nyq
 
@@ -39,8 +24,6 @@ class LowPassFilter:
         return data_table
 
 
-# Class for Principal Component Analysis. We can only apply this when we do not have missing values (i.e. NaN).
-# For this we have to impute these first, be aware of this.
 class PrincipalComponentAnalysis:
 
     pca = []
@@ -58,33 +41,23 @@ class PrincipalComponentAnalysis:
             )
         return dt_norm
 
-    # Perform the PCA on the selected columns and return the explained variance.
     def determine_pc_explained_variance(self, data_table, cols):
 
-        # Normalize the data first.
         dt_norm = self.normalize_dataset(data_table, cols)
 
-        # perform the PCA.
         self.pca = PCA(n_components=len(cols))
         self.pca.fit(dt_norm[cols])
-        # And return the explained variances.
         return self.pca.explained_variance_ratio_
 
-    # Apply a PCA given the number of components we have selected.
-    # We add new pca columns.
     def apply_pca(self, data_table, cols, number_comp):
 
-        # Normalize the data first.
         dt_norm = self.normalize_dataset(data_table, cols)
 
-        # perform the PCA.
         self.pca = PCA(n_components=number_comp)
         self.pca.fit(dt_norm[cols])
 
-        # Transform our old values.
         new_values = self.pca.transform(dt_norm[cols])
 
-        # And add the new ones:
         for comp in range(0, number_comp):
             data_table["pca_" + str(comp + 1)] = new_values[:, comp]
 
